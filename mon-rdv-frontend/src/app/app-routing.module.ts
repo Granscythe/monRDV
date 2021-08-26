1af39eec9e79d55f67703df6f2e71647b5833542
Mathieu W <w.mathieu.dk59@gmail.com>
2021-08-26 17:35:16 +0200

    planning html v1

--- mon-rdv-frontend/src/app/app-routing.module.ts
+++ mon-rdv-frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {PlanningComponent} from "./front/planning/planning.component";
 
 
 const routes: Routes = [
   // {path: "accueil", component: AccueilComponent},
+  {path: "planning", component: PlanningComponent},
   // {path: "", redirectTo: "accueil", pathMatch: "full"}
 ];
 
