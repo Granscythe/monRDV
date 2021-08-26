36606df3d6ae5c12f26efb1b32ea2009fe83c147
Xavier <xavier.darcissac@gmail.com>
2021-08-26 15:35:08 +0200

    1

--- mon-rdv-frontend/src/app/app.module.ts
+++ mon-rdv-frontend/src/app/app.module.ts
@@ -3,19 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EvaluationComponent } from './evaluation/evaluation.component';
-import {EvaluationService} from "./evaluation/evaluation.service";
-import { AccueilComponent } from './accueil/accueil.component';
 import {FormsModule} from "@angular/forms";
-import {StagiaireComponent} from "./stagiaire/stagiaire.component";
-import {StagiaireService} from "./stagiaire/stagiaire.service";
 import {AgePipe} from "./age.pipe";
-import {FiliereComponent} from "./filiere/filiere.component";
-import {FiliereService} from "./filiere/filiere.service";
+
 import {HttpClientModule} from "@angular/common/http";
-import {MatiereComponent} from "./matiere/matiere.component";
-import {FormateurComponent} from "./formateur/formateur.component";
-import {UeComponent} from "./ue/ue.component";
 import {FrontModule} from "./front/front.module";
 
 @NgModule({
