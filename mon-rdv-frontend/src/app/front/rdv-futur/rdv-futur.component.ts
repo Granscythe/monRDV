3dfd6be5575234c8736983f1661ae053df618ec0
Xavier <xavier.darcissac@gmail.com>
2021-08-27 13:07:10 +0200

    a

--- mon-rdv-frontend/src/app/front/rdv-futur/rdv-futur.component.ts
+++ mon-rdv-frontend/src/app/front/rdv-futur/rdv-futur.component.ts
@@ -13,19 +13,21 @@ import {CreneauHttpService} from "../creneau/creneau-http.service";
 })
 export class RdvFuturComponent implements OnInit {
 
-  consultations:Array<Consultation>=new  Array<Consultation>();
-  crenaux:Array<Creneau>=new Array<Creneau>();
-  patient:Patient;
+  consultations: Array<Consultation> = new Array<Consultation>();
+  crenaux: Array<Creneau> = new Array<Creneau>();
+  patient: Patient;
 
-  constructor(private rdvFuturService:RdvFuturHttpService,private creneauService:CreneauHttpService ) { }
+  constructor(private rdvFuturService: RdvFuturHttpService, private creneauService: CreneauHttpService) {
+  }
 
-  ngOnInit(): void {this.listCreneau(2);
+  ngOnInit(): void {
+    this.listCreneau(2);
 
   }
 
-  listCreneau(id:number){
-    this.creneauService.findAllByIdPatient(id).subscribe(resp=>{
-      this.crenaux=resp;
+  listCreneau(id: number) {
+    this.creneauService.findAllByIdPatient(id).subscribe(resp => {
+      this.crenaux = resp;
     });
   }
 
