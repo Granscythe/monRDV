7e23384720032a88c164efd0ded6b5395ed3c22d
Xavier <xavier.darcissac@gmail.com>
2021-08-27 10:40:54 +0200

    3.15

--- mon-rdv-frontend/src/app/front/validationRdv/validationRdv.component.ts
+++ mon-rdv-frontend/src/app/front/validationRdv/validationRdv.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ValidationRdvHttpService} from "./validationRdvHttp.service";
+import {Patient} from "../../model/patient";
+import {PatientHttpService} from "../patient/patient-http.service";
 
 @Component({
   selector: 'validationRdv',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ValidationRdvComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private patientService: PatientHttpService) {
+  }
 
   ngOnInit(): void {
   }
 
+  listPatient(): Array<Patient> {
+    return this.patientService.findAll();
+  }
+
+  add() {
+    this.validationForm = new ValidationRdv();
+  }
+
+  edit(id: number) {
+    this.validationService.findById(id).subscribe(resp => {
+      this.validationForm = resp;
+    })
+  }
+
+  save() {
+    if (this.validationForm.id) {
+      this.validationService.modify(this.validationForm);
+    } else {
+      this.validationService.create(this.validationForm);
+    }
+
+    this.validationForm = null;
+  }
+
+  // pour l'exemple => mais de préférence coder le subscribe dans le service
+  delete(id: number) {
+    this.validationService.deleteById(id).subscribe(resp => {
+      this.validationService.load();
+    }, error => console.log(error));
+  }
+
+  cancel() {
+    this.validationForm = null;
+  }
 }
