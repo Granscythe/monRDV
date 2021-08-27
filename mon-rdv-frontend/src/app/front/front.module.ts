a1bfeb48e43d09dd3c349c840640b598508b2e86
Xavier <xavier.darcissac@gmail.com>
2021-08-27 14:09:53 +0200

    a

--- mon-rdv-frontend/src/app/front/front.module.ts
+++ mon-rdv-frontend/src/app/front/front.module.ts
@@ -4,7 +4,6 @@ import { PlanningComponent } from './planning/planning.component';
 import {RdvFuturComponent} from "./rdv-futur/rdv-futur.component";
 import {ConsultationComponent} from "./consultation/consultation.component";
 import { CreneauComponent } from './creneau/creneau.component';
-import {AppModule} from "../app.module";
 import { RdvPasserComponent } from './rdv-passer/rdv-passer.component';
 import {StatutCatePipe} from "./statut-cate.pipe";
 import {PatientComponent} from "./patient/patient.component";
