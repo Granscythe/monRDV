2b62a3f5fcc183caaca6ea49981eb8a52985483e
Pierre <pierre.grandcoin@gadz.org>
2021-08-27 10:56:30 +0200

    planningDTO model

--- mon-rdv-frontend/src/app/model/planningDTO.ts
+++ mon-rdv-frontend/src/app/model/planningDTO.ts
@@ -1,6 +1,13 @@
+import {Creneau} from "./Creneau";
+import {Praticien} from "./praticien";
 
 export class PlanningDTO {
 
+  praticien: Praticien;
+  creneaux: Array<Creneau>;
 
-
+  constructor(praticien: Praticien, creneaux: Array<Creneau>) {
+    this.praticien = praticien;
+    this.creneaux = creneaux;
+  }
 }
